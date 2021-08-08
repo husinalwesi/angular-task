d1b8ad6ff4302680d0b686cd59267bf5bbacdeab
Your Name <you@example.com>
2021-08-08 18:39:03 +0300

    create creative side menu

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { mainMenuLinksInterface } from '../../interfaces/general';
 
 @Component({
   selector: 'app-header',
@@ -6,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  isNavbarCollapsed: boolean = false;
+  mainMenuLinks: mainMenuLinksInterface[] = [//Dummy data.
+    { title: "home", path: "" },
+    { title: "chill adventure", path: "/offers/chill-adventure" },
+    { title: "spooky times", path: "/offers/spooky-times" },
+    { title: "desert madness", path: "/offers/desert-madness" },
+    { title: "out in the wild", path: "/offers/out-in-the-wild" }
+  ];
   constructor() { }
 
   ngOnInit(): void {
--- src/app/shared/interfaces/general.ts
+++ src/app/shared/interfaces/general.ts
@@ -2,3 +2,8 @@ export interface socialShareInterface {
     key: string;
     url: string;
 }
+
+export interface mainMenuLinksInterface {
+    title: string;
+    path: string;
+}
