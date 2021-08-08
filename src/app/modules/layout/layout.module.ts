eb511d15baab7c91cd70f42552157b73495b56d7
Your Name <you@example.com>
2021-08-08 12:25:22 +0300

    slicing animated-background-light and trip-type section and link it

--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -6,10 +6,12 @@ import { SharedModule } from '../../shared/shared.module';
 // Components
 import { MainHomeSectionComponent } from './components/main-home-section/main-home-section.component';
 import { TripSectionComponent } from './components/trip-section/trip-section.component';
+import { AnimatedLightBackgroundComponent } from './components/animated-light-background/animated-light-background.component';
+import { TripTypeComponent } from './components/trip-type/trip-type.component';
 // 
 @NgModule({
-  declarations: [MainHomeSectionComponent, TripSectionComponent],
+  declarations: [MainHomeSectionComponent, TripSectionComponent, AnimatedLightBackgroundComponent, TripTypeComponent],
   imports: [CommonModule, LayoutRoutingModule, SharedModule],
-  exports: [MainHomeSectionComponent, TripSectionComponent]
+  exports: [MainHomeSectionComponent, TripSectionComponent, AnimatedLightBackgroundComponent, TripTypeComponent]
 })
 export class LayoutModule {}
